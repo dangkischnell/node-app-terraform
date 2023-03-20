3c25a08d3874a7b96ce73b3e99122b1fced37a4f
Your Name <you@example.com>
2023-03-20 17:34:00 +0100

    v

--- node-app.tf
+++ node-app.tf
@@ -1,11 +1,11 @@
-# variable "access_key" {}
-# variable "secret_key" {}
+variable "access_key" {}
+variable "secret_key" {}
 terraform {
   required_version = ">= 0.14"
 }
 provider "aws" {
-  # access_key = "${var.access_key}"
-  # secret_key = "${var.secret_key}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
   region     = "eu-central-1"
 }
 
